b0bae8124615512e3f79ee48ee7a59faa096910a
rozik <rozik4356@mail.ru>
2021-01-21 15:14:56 +0600

    added search

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,28 @@ $(function(){
         fade: true,
         autoplay: true,
         autoplaySpeed: 3000
+    });
+
+
+    const searchBtn = $('.search__btn');
+    const cancelBtn = $('.search__cancel');
+    const searchBox = $('.search');
+    const searchInput = $('.search__input');
+
+    searchBtn.on('click', function(){
+        searchBox.addClass('active');
+        searchInput.addClass('active');
+        searchBtn.addClass('active');
+        cancelBtn.addClass('active');
     })
+
+    cancelBtn.on('click', function(){
+        searchBox.removeClass('active');
+        searchInput.removeClass('active');
+        searchBtn.removeClass('active');
+        cancelBtn.removeClass('active');
+    })
+
+
 
 });
